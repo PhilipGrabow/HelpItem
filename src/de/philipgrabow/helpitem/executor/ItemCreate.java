8558e5e5cc2ec60652bc0982806b6f32a53f5c3a
PhilipGrabow <philip.grabow@github.com>
2017-03-17 20:20:50 +0100

    Abfrage zum Item Erstellen hinzugefügt

--- src/de/philipgrabow/helpitem/executor/ItemCreate.java
+++ src/de/philipgrabow/helpitem/executor/ItemCreate.java
@@ -1,5 +1,7 @@
 package de.philipgrabow.helpitem.executor;
 
+import java.util.ArrayList;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,28 +21,36 @@ public class ItemCreate implements CommandExecutor {
 		if (cmd.getName().equalsIgnoreCase("helpitem")) {
 			if (sender instanceof Player) {
 				Player p = (Player) sender;
-				int i = 0;
+				ArrayList<String> list = new ArrayList<String>();
 				if (args.length == 0) {
 					if (p.hasPermission("helpitem.create.self")) {
-						TextComponent message = new TextComponent("<JA>");
-						message.setColor(ChatColor.GREEN);
-						TextComponent message1 = new TextComponent("<NEIN>");
-						message1.setColor(ChatColor.RED);
-						message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/helpitem w"));
-						message1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/helpitem s"));
-						message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM,new ComponentBuilder("Klicke hier für JA!").create()));
-						message1.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM,new ComponentBuilder("Klicke hier für NEIN!").create()));
-						message.addExtra(message1);
-						p.sendMessage("§cWillst du dieses Item wirklich?");
-						p.sendMessage(ChatColor.YELLOW + "Klicke JA oder NEIN unter diesem Text!");
-						i = 1;
-						p.spigot().sendMessage(message);
-						return true;
+						if (list.contains(p.getName())) {
+							p.sendMessage("Beende erst den Vorherigen Vorgang!");
+							return true;
+						} else {
+							TextComponent message = new TextComponent("<JA>");
+							message.setColor(ChatColor.GREEN);
+							TextComponent message1 = new TextComponent("<NEIN>");
+							message1.setColor(ChatColor.RED);
+							message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/helpitem w"));
+							message1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/helpitem s"));
+							message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM,
+									new ComponentBuilder("Klicke hier für JA!").create()));
+							message1.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM,
+									new ComponentBuilder("Klicke hier für NEIN!").create()));
+							message.addExtra(message1);
+							p.sendMessage("§cWillst du dieses Item wirklich?");
+							p.sendMessage(ChatColor.YELLOW + "Klicke JA oder NEIN unter diesem Text!");
+							list.add(p.getName());
+							p.spigot().sendMessage(message);
+							return true;
+						}
 					}
 				}
 				if (args[0].equalsIgnoreCase("w")) {
-					if (i == 1) {
+					if(list.contains(p.getName())) {
 						Item.createItem(p);
+						list.remove(p.getName());
 						p.sendMessage("Item erfolgreich erstellt!");
 						return true;
 					} else {
@@ -49,8 +59,9 @@ public class ItemCreate implements CommandExecutor {
 					}
 				}
 				if (args[0].equalsIgnoreCase("s")) {
-					if (i == 1) {
+					if(list.contains(p.getName())) {
 						p.sendMessage("Vorgang abgebrochen!");
+						list.remove(p.getName());
 						return true;
 					} else {
 						p.sendMessage("Kein Vorgang vorhanden!");
